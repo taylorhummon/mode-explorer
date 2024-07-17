a49d3d3a265d298505abb8139bade8e5c0ecb53d
Taylor Hummon <taylor.hummon@gmail.com>
2024-07-17 13:14:00 -0400

    prefer double quotes to single quotes

--- src/specs/components/ModeExplorer.spec.js
+++ src/specs/components/ModeExplorer.spec.js
@@ -1,7 +1,7 @@
-import { render } from 'inferno';
-import ModeExplorer from '../../components/ModeExplorer';
+import { render } from "inferno";
+import ModeExplorer from "../../components/ModeExplorer";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   render(<ModeExplorer />, div);
 });
