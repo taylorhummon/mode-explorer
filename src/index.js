4c8bfc8e0dfaed8739f9ecf1be1ec3b8030fe709
Taylor Hummon <taylor.hummon@gmail.com>
2024-07-20 15:16:30 -0400

    update component mount point

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import ModeExplorer from "/src/components/ModeExplorer.js";
 
-const container = document.getElementById("mode-explorer");
-const root = createRoot(container)
+const container = document.getElementById("mode-explorer-mount-point");
+const root = createRoot(container);
 root.render(<ModeExplorer />);
