5bf1706510ef6767d35b9ef7cb7a3eff4aeeb5c6
Taylor Hummon <taylor.hummon@gmail.com>
2024-07-24 08:37:33 -0400

    type checking

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,7 @@ import { createRoot } from "react-dom/client";
 import ModeExplorer from "./components/ModeExplorer";
 
 const container = document.getElementById("mode-explorer-mount-point");
-const root = createRoot(container);
-root.render(<ModeExplorer />);
+if (container) {
+  const root = createRoot(container);
+  root.render(<ModeExplorer />);
+}
