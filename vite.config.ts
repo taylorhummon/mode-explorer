63aeb80d03c2cab48edf2c49a37aefad661195ff
Taylor Hummon <taylor.hummon@gmail.com>
2024-08-13 10:20:06 -0400

    add a minimal entry point

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,9 +15,16 @@ export default defineConfig({
     port: 1234,
     strictPort: true
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: "minimal.html"
+      },
+    },
+  },
   test: {
     dir: "tests",
     environment: "jsdom",
-    setupFiles: './tests/setupTests.ts'
+    setupFiles: "./tests/setupTests.ts"
   }
 });
