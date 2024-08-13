6f2159227fdd2098d94c186f26d3631d0a630182
Taylor Hummon <taylor.hummon@gmail.com>
2024-08-13 11:07:01 -0400

    use strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import ModeExplorer from "src/components/ModeExplorer";
@@ -5,4 +6,8 @@ import ModeExplorer from "src/components/ModeExplorer";
 
 const container = document.getElementById("mode-explorer-mount-point") as HTMLElement;
 const root = createRoot(container);
-root.render(<ModeExplorer />);
+root.render(
+  <StrictMode>
+    <ModeExplorer />
+  </StrictMode>
+);
