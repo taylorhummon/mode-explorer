ee432505924df61fa893037a784fbd50c1f963ef
Taylor Hummon <other@taylorhummon.com>
2026-03-06 10:08:39 -0500

    display the mode name

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -2,6 +2,7 @@ import type { Derived, Move } from "../types";
 import { Motion } from "../enumerations";
 import Clock from "./Clock";
 import ModeNote from "./ModeNote";
+import ModeName from "./ModeName";
 import SolfegeDot from "./SolfegeDot";
 import SolfegeLabel from "./SolfegeLabel"
 import Arrow from "./Arrow";
@@ -18,43 +19,49 @@ export default function Canvas({
   buildMove
 }: CanvasProps): JSX.Element {
   return (
-    <svg
-      viewBox="-150 -150 300 300"
-      xmlns="http://www.w3.org/2000/svg"
-      height="300px"
-      width="300px"
-    >
-      <Clock />
-      <ModeNote
-        derived={derived}
-      />
-      {arrayFromMap(derived.solfegeByName, (solfege, name) => (
-        <SolfegeDot
-          key={name}
-          name={name}
-          location={solfege.location}
-          move={buildMove(solfege.availableMotion)}
+    <>
+      <svg
+        viewBox="-150 -150 300 300"
+        xmlns="http://www.w3.org/2000/svg"
+        height="300px"
+        width="300px"
+      >
+        <Clock />
+        <ModeNote
+          derived={derived}
         />
-      ))}
-      {arrayFromMap(derived.solfegeByName, (solfege, name) => (
-        <SolfegeLabel
-          key={name}
-          name={name}
-          location={solfege.location}
+        {arrayFromMap(derived.solfegeByName, (solfege, name) => (
+          <SolfegeDot
+            key={name}
+            name={name}
+            location={solfege.location}
+            move={buildMove(solfege.availableMotion)}
+          />
+        ))}
+        {arrayFromMap(derived.solfegeByName, (solfege, name) => (
+          <SolfegeLabel
+            key={name}
+            name={name}
+            location={solfege.location}
+          />
+        ))}
+        <Arrow
+          isAdvance={true}
+          hour={derived.advanceableHour}
+          isHidden={derived.isAnimating}
+          move={buildMove(Motion.AdvanceIndividual)}
         />
-      ))}
-      <Arrow
-        isAdvance={true}
-        hour={derived.advanceableHour}
+        <Arrow
+          isAdvance={false}
+          hour={derived.retreatableHour}
+          isHidden={derived.isAnimating}
+          move={buildMove(Motion.RetreatIndividual)}
+        />
+      </svg>
+      <ModeName
+        modeIndex={derived.modeIndex}
         isHidden={derived.isAnimating}
-        move={buildMove(Motion.AdvanceIndividual)}
       />
-      <Arrow
-        isAdvance={false}
-        hour={derived.retreatableHour}
-        isHidden={derived.isAnimating}
-        move={buildMove(Motion.RetreatIndividual)}
-      />
-    </svg>
+    </>
   )
 };
